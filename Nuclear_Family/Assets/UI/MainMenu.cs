a1e33cd60cf954a0dbc87ca2e68b2aa5417174e2
Nico Adrogue <nicholas.adrogue@tufts.edu>
2021-11-16 01:41:02 -0500

    Main/Pause Menu

---
+++ Nuclear_Family/Assets/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour  {
+
+        public GameObject mainMenuUI;
+
+        public void Start2(){
+                Time.timeScale = 1f;
+                SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+SceneManager.LoadScene ("SampleScene");
+        }
+
+      public void QuitGame2() {
+                #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                Application.Quit();
+                #endif
+      }
+}
