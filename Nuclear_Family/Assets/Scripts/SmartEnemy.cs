8fb19eb33bd98fda4d4a9e7969d16e0b85404f5b
theoforget <theo.forget@tufts.edu>
2021-12-07 17:27:05 -0500

    Started the Smart Enemy Script

--- Nuclear_Family/Assets/Scripts/SmartEnemy.cs
+++ Nuclear_Family/Assets/Scripts/SmartEnemy.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SmartEnemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class SmartEnemy : MonoBehaviour {
+    
+	public int maxHealth = 100;
+    public int current_health;
+    public HealthBar healthBar;
+    public float speed;
+    public bool omnidirectional;
+    private GameObject rick;
+    private Player playerscript;
+    // public Animator animator = GetComponent<Animator>();
+    // public GameObject deathEffect;
+    
+    void Start() {
+        current_health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        playerscript = GameObject.Find("Rick").GetComponent<Player>();
     }
+    
+    void Update() {
+        float X = playerscript._Blade.transform.position.x;
+        float Y = 0f;
+        if (omnidirectional == true) {
+            Y = playerscript._Blade.transform.position.x;
+        }
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(X,Y), speed * Time.deltaTime);
+    }
+    
+	public void TakeDamage (int damage) 
+    { 
+        current_health -= damage;
+        healthBar.SetHealth(current_health);
+		if (current_health <= 0)
+        {
+            Die();
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Die() 
+    { 
+        // Instantiate(deathEffect, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+	}
 }
