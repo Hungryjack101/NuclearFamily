82d437c60de82a0eef41fd6a8069c62f4adba219
theoforget <theo.forget@tufts.edu>
2021-12-21 12:57:00 -0800

    Final Level Implemented with New Enemies

--- Nuclear_Family/Assets/Scripts/Dialogue.cs
+++ Nuclear_Family/Assets/Scripts/Dialogue.cs
@@ -22,7 +22,7 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) {
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Return)) {
             if (textComponent.text == lines[index]) {
                 NextLine();
             } else {
