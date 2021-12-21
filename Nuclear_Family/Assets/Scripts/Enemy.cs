276dc4f3c090e916356d3e7ab60af0bdbafc3b22
theoforget <theo.forget@tufts.edu>
2021-12-21 11:15:14 -0800

    Fixed Animation

--- Nuclear_Family/Assets/Scripts/Enemy.cs
+++ Nuclear_Family/Assets/Scripts/Enemy.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
     
-	public int maxHealth = 100;
+    private float dirX;
+    public float speed;
+    private Rigidbody2D rb;
+    private bool facingRight = false;
+    private Vector2 localScale;
+    
+    public int maxHealth = 100;
     public int current_health;
     public HealthBar healthBar;
     public float min;
     public float max;
-    public float speed;
     public bool updown;
     
-    void Start() {
+    public float coolDown = 1;
+    public float current_cool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        dirX = -1f;
         current_health = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         if (updown) {
@@ -25,13 +38,54 @@ public class Enemy : MonoBehaviour {
     }
     
     void Update() {
-        if (updown) {
-            transform.position = new Vector2(transform.position.x, Mathf.PingPong(Time.time*speed,max-min)+min);
-        } else {    
-            transform.position = new Vector2(Mathf.PingPong(Time.time*speed,max-min)+min, transform.position.y);
+        if (current_cool > 0) {
+            current_cool -= Time.deltaTime;
+        } else {
+            current_cool = 0;
+        }
+        if (updown && current_cool == 0) {
+            if (transform.position.y > max || transform.position.y < min) {
+                dirX *= -1f;
+                current_cool = coolDown;
+            }
+        } else if (!updown && current_cool == 0){
+            if (transform.position.x > max || transform.position.x < min) {
+                dirX *= -1f;
+                current_cool = coolDown;
+            }
         }
     }
-	public void TakeDamage (int damage) 
+
+    void FixedUpdate()
+    {
+        if (updown) {
+            rb.velocity = new Vector2(rb.velocity.x, dirX * speed);
+        } else {
+            rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+        }
+    }
+
+    void LateUpdate()
+    {
+        CheckWhereToFace();
+    }
+
+    void CheckWhereToFace()
+    {
+        if (!updown) {
+            if (dirX > 0)
+                facingRight = true;
+            else if (dirX < 0)
+                facingRight = false;
+
+            if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+                localScale.x *= -1;
+
+            transform.localScale = localScale;
+        }
+    }
+    
+    public void TakeDamage (int damage) 
     { 
         current_health -= damage;
         healthBar.SetHealth(current_health);
@@ -47,3 +101,47 @@ public class Enemy : MonoBehaviour {
 		Destroy(gameObject);
 	}
 }
+// 
+// public int maxHealth = 100;
+//     public int current_health;
+//     public HealthBar healthBar;
+//     public float min;
+//     public float max;
+//     public float speed;
+//     public bool updown;
+// 
+//     void Start() {
+//         current_health = maxHealth;
+//         healthBar.SetMaxHealth(maxHealth);
+//         if (updown) {
+//             min=transform.position.y-min;
+//             max=transform.position.y+max;
+//         } else {
+//             min=transform.position.x-min;
+//             max=transform.position.x+max;
+//         }
+//     }
+// 
+//     void Update() {
+//         if (updown) {
+//             transform.position = new Vector2(transform.position.x, Mathf.PingPong(Time.time*speed,max-min)+min);
+//         } else {    
+//             transform.position = new Vector2(Mathf.PingPong(Time.time*speed,max-min)+min, transform.position.y);
+//         }
+//     }
+// 	public void TakeDamage (int damage) 
+//     { 
+//         current_health -= damage;
+//         healthBar.SetHealth(current_health);
+// 		if (current_health <= 0)
+//         {
+//             Die();
+// 		}
+// 	}
+// 
+// 	void Die() 
+//     { 
+//         // Instantiate(deathEffect, transform.position, Quaternion.identity);
+// 		Destroy(gameObject);
+// 	}
+// }
